19d5a4301184b5f947b750ef052da4b548ae4690
eric-ward <eric.ward@noaa.gov>
2017-02-02 13:43:00 -0800

    do rotation for every MCMC sample to better propagate uncertainty

--- rotate_trends.R
+++ rotate_trends.R
@@ -2,14 +2,26 @@ rotate_trends = function(fitted_model) {
   
   # Illustrate how to get the trends out of the model
   # get the inverse of the rotation matrix
-  Zest = apply(extract(fitted_model)$Z, c(2,3), mean)
-  H.inv = varimax(Zest)$rotmat
+  n_mcmc = dim(extract(fitted_model)$Z)[1]
+  Z = extract(fitted_model)$Z
+  x = extract(fitted_model)$x
+  n_ts = dim(Z)[2]
+  n_trends = dim(x)[2]
+  n_years = dim(x)[3]
+  mcmc_trends_rot = array(0, dim = c(n_mcmc, n_trends, n_years))
+  mcmc_Z_rot = array(0, dim = c(n_mcmc, n_ts, n_trends))
+  for(i in 1:n_mcmc) {
+    Zest = Z[i,,]
+    H.inv = varimax(Zest)$rotmat
   
-  # rotate factor loadings
-  Z.rot = Zest %*% H.inv
-  # rotate trends
-  states = apply(extract(fitted_model)$x, c(2,3), mean)
-  trends.rot = solve(H.inv) %*% states
-  
-  return(list("Z_rot"=Z.rot, "trends"=t(trends.rot)))
+    # rotate factor loadings
+    Z.rot = Zest %*% H.inv
+    mcmc_Z_rot[i,,] = Z.rot
+    
+    # rotate trends
+    states = x[i,,]
+    trends.rot = solve(H.inv) %*% states
+    mcmc_trends_rot[i,,] = trends.rot
+  }
+  return(list("Z_rot"=mcmc_Z_rot, "trends"=mcmc_trends_rot))
 }
