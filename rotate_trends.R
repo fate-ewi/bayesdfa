dbb03618529f15e9a5ca0a6d2e406f4b0a3d2188
jin-gao <jingao84@gmail.com>
2017-02-02 16:15:36 -0800

    minor

--- rotate_trends.R
+++ rotate_trends.R
@@ -24,8 +24,8 @@ rotate_trends = function(fitted_model) {
     mcmc_trends_rot[i,,] = trends.rot
   }
   return(list("Z_rot"=mcmc_Z_rot, "trends"=mcmc_trends_rot,
-    "Z_rot_mean" = apply(mcmc_Z_rot,c(2,3),mean)),
-    "trends_mean" = apply(mcmc_trends_rot,c(2,3),mean)),
-  "trends_lower" = apply(mcmc_trends_rot,c(2,3),quantile,0.025)),
+    "Z_rot_mean" = apply(mcmc_Z_rot,c(2,3),mean),
+    "trends_mean" = apply(mcmc_trends_rot,c(2,3),mean),
+  "trends_lower" = apply(mcmc_trends_rot,c(2,3),quantile,0.025),
 "trends_upper" = apply(mcmc_trends_rot,c(2,3),quantile,0.975)))
 }
