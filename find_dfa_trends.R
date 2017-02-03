0b73fafd9c1fd4cdd6c8c3b7205b9ad65b164c1b
Jin Gao <jingao84@gmail.com>
2017-02-02 20:56:12 -0800

    minor

--- find_dfa_trends.R
+++ find_dfa_trends.R
@@ -3,7 +3,7 @@ find_dfa_trends = function(y = y, kmin=1, kmax=5, iter=2000) {
   df = data.frame("model"=seq(1,2*length(kmin:kmax)), "num_trends"=NA, "looic"=NA, "cor"=NA)
   indx = 1
   for(i in kmin:kmax) {
-  model = fit_dfa(y = y, num_trends = k, iter=iter)
+  model = fit_dfa(y = y, num_trends = i, iter=iter)
   df$num_trends[indx] = i
   df$looic[indx] = loo(extract_log_lik(model))$looic
   df$cor[indx] = "equal"
@@ -11,7 +11,7 @@ find_dfa_trends = function(y = y, kmin=1, kmax=5, iter=2000) {
   }
 
   for(i in kmin:kmax) {
-    model = fit_dfa(y = y, num_trends = k, iter=iter, varIndx = seq(1,nrow(y)))
+    model = fit_dfa(y = y, num_trends = i, iter=iter, varIndx = seq(1,nrow(y)))
     df$num_trends[indx] = i
     df$looic[indx] = loo(extract_log_lik(model))$looic
     df$cor[indx] = "independent"
