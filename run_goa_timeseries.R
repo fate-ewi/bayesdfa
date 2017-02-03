d51d31891e4c4e343b9368c45ff963d0c606b472
eric-ward <eric.ward@noaa.gov>
2017-02-02 19:45:20 -0800

    dfa_prior model
    This is the limited case where you have data split in 2 pieces, and
    want to update the loadings. Posterior from first model used as priors
    in 2nd.

--- run_goa_timeseries.R
+++ run_goa_timeseries.R
@@ -19,11 +19,10 @@ ak_full = ak_full[,which(names(ak_full)%in%meta$Code)]
 
 for(i in 1:ncol(ak_full)) ak_full[,i] = as.numeric(ak_full[,i])
 
-mod1 = fit_dfa(y = t(ak_full), num_trends = 2, iter=200)
+df = find_dfa_trends(y = t(ak_full), iter=2000)
+
+mod1 = fit_dfa(y = t(ak_full), num_trends = 2, iter=2000)
 rt = rotate_trends(mod1)
-
-df = find_dfa_trends(y = t(ak_full), iter=200)
-
 
 
 par(mfrow = c(2,2), mai=c(0.5,0.7,0.1,0.1))
@@ -58,4 +57,61 @@ ggplot(df, aes(year, mean)) +
   facet_wrap(~trend) + geom_line()
 
 
+# 2-stage model -- let's assume there's 3 trends for data
+dfa1 = fit_dfa(y = t(ak_full[1:32,]), num_trends = 3, varIndx = seq(1,ncol(ak_full)))
+zz_mean = apply(extract(dfa1)$Z, c(2,3), mean)
+zz_sd = apply(extract(dfa1)$Z, c(2,3), sd)
+
+z_mean = zz_mean[which(mat_indx != 0)]
+z_sd = zz_sd[which(mat_indx != 0)]
+
+data_list = list(
+  N,
+  P,
+  K,
+  nZ,
+  y,
+  row_indx,
+  col_indx,
+  nZero,
+  row_indx_z,
+  col_indx_z,
+  nZero,
+  row_indx_z,
+  col_indx_z,
+  row_indx_pos,
+  col_indx_pos,
+  n_pos,
+  z_mean,
+  z_sd
+)
+
+mod = stan(
+  data = data_list,
+  pars = c("x", "Z", "sigma", "log_lik"),
+  file = "dfa_prior.stan",
+  chains = chains,
+  iter = iter,
+  control = control
+)
+
+zz2_mean = apply(extract(mod)$Z, c(2,3), mean)
+zz2_sd = apply(extract(mod)$Z, c(2,3), sd)
+
+cor(zz_mean[,1], zz2_mean[,1])
+cor(zz_sd[,1], zz2_sd[,1])
+
+N = ncol(y)
+P = nrow(y)
+K = num_trends # number of dfa trends
+nZ = P * K - sum(1:K) + K
+mat_indx = matrix(0, P, 3)
+start = 1
+for (k in 1:3) {
+  if (k == 1)
+    mat_indx[, k] = (1:nZ)[start:(start + P - k)]
+  if (k > 1)
+    mat_indx[-c(0:(k - 1)), k] = (1:nZ)[start:(start + P - k)]
+  start = start + (P - k + 1)
+}
 
