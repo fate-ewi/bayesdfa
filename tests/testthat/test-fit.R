b5b99a505e3868fa23b2e3d057d0aac0ab133354
Sean Anderson <sean@seananderson.ca>
2017-02-24 13:01:39 -0800

    Add unit tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -1,11 +1,17 @@
 library(MARSS)
-if (interactive()) {
-  options(mc.cores = parallel::detectCores())
-}
-
 data(harborSealWA)
 y <- t(harborSealWA[, c("SJF", "SJI", "EBays", "PSnd")])
 
-fit1 <- fit_dfa(y = y, num_trends = 1)
+set.seed(1)
 
-testthat::expect_output(print(fit1), "Inference for Stan model")
+test_that("Basic model fits", {
+  fit1 <- fit_dfa(y = y, num_trends = 1)
+  expect_output(print(fit1), "Inference for Stan model")
+})
+
+test_that("find_dfa_trends works", {
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+  dfa_summary <- find_dfa_trends(y = y, iter = 1000, kmin = 1, kmax = 1)
+})
