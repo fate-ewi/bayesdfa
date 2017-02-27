045e990a2bf69455933d800c0f6b193a061e6819
Sean Anderson <sean@seananderson.ca>
2017-02-27 13:10:19 -0800

    Simplify loading plots; show credible intervals

--- R/plot_loadings.R
+++ R/plot_loadings.R
@@ -20,10 +20,10 @@
 #'
 #' @examples
 #' y <- t(MARSS::harborSealWA[, c("SJF", "SJI", "EBays", "PSnd")])
-#' m <- fit_dfa(y = y, num_trends = 2, iter = 500)
+#' m <- fit_dfa(y = y, num_trends = 2, iter = 2000)
 #' r <- rotate_trends(m)
-#' plot_loadings(r, threshold = 0.01, facet = TRUE)
-#' plot_loadings(r, threshold = 0.01, facet = FALSE)
+#' plot_loadings(r, threshold = 0.01, violin = FALSE, facet = TRUE)
+#' plot_loadings(r, threshold = 0.01, violin = FALSE, facet = FALSE)
 #' plot_loadings(r, threshold = 0.01, violin = TRUE, facet = FALSE)
 #' plot_loadings(r, threshold = 0.01, violin = TRUE, facet = TRUE)
 
@@ -31,38 +31,34 @@ plot_loadings = function(rotated_modelfit,
   names = NULL,
   threshold = 0.8,
   facet = TRUE,
-  violin = FALSE) {
+  violin = TRUE,
+  conf_level = 0.95) {
 
-  # rotate the trends
-  rotated <- rotated_modelfit
+  v <- reshape2::melt(rotated_modelfit$Z_rot, varnames = c("iter", "name", "trend"))
+  v$trend <- paste0("Trend ", v$trend)
+  v$trend <- as.factor(v$trend)
+  if (!is.null(names)) v$name <- names[v$name]
+  v$name <- as.factor(v$name)
 
-  n_ts <- dim(rotated$Z_rot)[2]
-  if (is.null(names)) {
-    names <- paste0("ts_", 1:n_ts)
-  }
-  n_trends <- dim(rotated$Z_rot)[3]
+  vsum <- dplyr::group_by_(v, "name", "trend")
+  vsum <- dplyr::summarize(vsum,
+    lower = quantile(value, probs = (1 - conf_level) / 2),
+    upper = quantile(value, probs = 1 - (1 - conf_level) / 2),
+    median = median(value))
+  df <- dplyr::ungroup(vsum)
 
-  # convert to df for ggplot
-  df <- data.frame(
-    x = c(rotated$Z_rot_mean),
-    trend = paste0("Trend ", sort(rep(seq_len(n_trends), n_ts))),
-    name = rep(names, n_trends),
-    lower = c(rotated$Z_rot_mean) - c(apply(rotated$Z_rot,c(2,3),sd)),
-    upper = c(rotated$Z_rot_mean) + c(apply(rotated$Z_rot,c(2,3),sd)),
-    q_lower = c(apply(rotated$Z_rot,c(2,3),function(x) {return(length(which(x<0))/length(x))}))
-  )
-  df$q_upper <- 1 - df$q_lower
+  # q_lower = c(apply(rotated$Z_rot,c(2,3),function(x) {return(length(which(x<0))/length(x))}))
+  # df$q_upper <- 1 - df$q_lower
 
   # replace low values with NAs
-  df$x <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$x, NA)
-  df$lower <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$lower, NA)
-  df$upper <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$upper, NA)
+  # df$x <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$x, NA)
+  # df$lower <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$lower, NA)
+  # df$upper <- ifelse((df$q_lower > threshold | df$q_upper > threshold), df$upper, NA)
 
-  # make faceted ribbon plot of trends
-  df <- df[!is.na(df$x), ]
+  # df <- df[!is.na(df$x), ]
 
   if (!violin) {
-    p1 <- ggplot(df, aes_string(x = "name", y = "x", col = "trend")) +
+    p1 <- ggplot(df, aes_string(x = "name", y = "median", col = "trend")) +
       geom_point(size = 3, alpha = 0.95, position = position_dodge(0.3)) +
       geom_errorbar(aes_string(ymin = "lower", ymax = "upper"),
         alpha = 0.6, position = position_dodge(0.3), width = 0) +
@@ -71,11 +67,7 @@ plot_loadings = function(rotated_modelfit,
   }
 
   if (violin) {
-    v <- reshape2::melt(rotated$Z_rot, varnames = c("iter", "ts_name", "trend"))
-    v$trend <- as.factor(v$trend)
-    if (!is.null(names)) v$ts_name <- names[v$ts_name]
-    v$ts_name <- as.factor(v$ts_name)
-    p1 <- ggplot(v, aes_string(x = "ts_name", y = "value", fill = "trend")) +
+    p1 <- ggplot(v, aes_string(x = "name", y = "value", fill = "trend")) +
       geom_violin(color = NA) +
       geom_hline(yintercept = 0, lty = 2) +
       coord_flip() + xlab("Time Series") + ylab("Loading")
