cbe67d7ff8f2cf8c2c1689b7dadd288a7304cd58
Sean Anderson <sean@seananderson.ca>
2017-04-20 14:43:06 -0700

    Add outlier detection in trends

---
+++ R/find_swans.R
@@ -0,0 +1,41 @@
+#' Find outlying "black swan" jumps in trends 
+#'
+#' @param rotated_modelfit Output from \code{\link{rotate_trends}}
+#' @param threshold A probability threshold below which to 
+#'   flag trend events as extreme 
+#' @examples
+#' \dontrun{
+#' y <- t(MARSS::harborSealWA[, c("SJF", "SJI", "EBays")])
+#' y[,10] <- y[,10] + 2
+#' set.seed(1)
+#' m <- fit_dfa(y = y, num_trends = 2, iter = 1000, chains = 1, nu_fixed = 2)
+#' r <- rotate_trends(m)
+#' p <- plot_trends(r)
+#' print(p)
+#' find_swans(r)
+#' }
+#' @export
+
+find_swans <- function(rotated_modelfit, threshold = 0.01) {
+  x <- rotated_modelfit$trends_mean
+  d <- apply(x, 1, function(xx) c(NA, diff(xx)))
+  prob <- 1 - apply(d, 2, function(xx) pnorm(abs(xx), 0, 1))
+  prob <- as.data.frame(prob)
+  trends <- as.data.frame(t(x))
+  trends$time <- seq_len(nrow(trends))
+  prob$time <- seq_len(nrow(prob))
+  trends <- reshape2::melt(trends, id.vars = c("time"))
+  prob <- reshape2::melt(prob, id.vars = c("time"))
+  names(trends) <- c("time", "trend_number", "trend_value")
+  names(prob) <- c("time", "trend_number", "probability")
+
+  trends <- dplyr::inner_join(trends, prob)
+  trends$trend_number <- as.numeric(sub("V", "", trends$trend_number))
+  trends$below_threshold <- trends$probability < threshold
+  trends
+  # ggplot(trends, aes(time, trend_value, color = below_threshold)) + geom_point() + facet_wrap(~trend_number)
+  # ggplot(trends, aes(time, probability, color = below_threshold)) + geom_point() + facet_wrap(~trend_number)
+ 
+}
+# library(mvtnorm)
+
--- R/plot_trends.R
+++ R/plot_trends.R
@@ -2,20 +2,27 @@
 #'
 #' @param rotated_modelfit Output from \code{\link{rotate_trends}}
 #' @param years Optional numeric vector of years for the plot
+#' @param highlight_outliers Logical. Should trend events 
+#'   that exceed the probability of occurring with a normal distribution as
+#'   defined by \code{threshold} be highlighted? 
+#' @param threshold A probability threshold below which to 
+#'   flag trend events as extreme 
 #'
 #' @export
 #' @seealso plot_loadings fit_dfa rotate_trends
 #'
-#' @importFrom ggplot2 geom_ribbon facet_wrap
+#' @importFrom ggplot2 geom_ribbon facet_wrap geom_point
 #'
 #' @examples
 #' y <- t(MARSS::harborSealWA[, c("SJF", "SJI", "EBays", "PSnd")])
-#' m <- fit_dfa(y = y, num_trends = 2, iter = 600)
+#' y[,10] <- y[,10] + 2
+#' m <- fit_dfa(y = y, num_trends = 2, iter = 600, nu_fixed = 5)
 #' r <- rotate_trends(m)
-#' p <- plot_trends(r)
+#' p <- plot_trends(r, highlight_outliers = TRUE)
 #' print(p)
 
-plot_trends = function(rotated_modelfit, years = NULL) {
+plot_trends = function(rotated_modelfit, years = NULL, 
+  highlight_outliers = FALSE, threshold = 0.01) {
   # rotate the trends
   rotated <- rotated_modelfit
 
@@ -38,6 +45,12 @@ plot_trends = function(rotated_modelfit, years = NULL) {
     geom_ribbon(aes_string(ymin = "lo", ymax = "hi"), alpha = 0.4) +
     geom_line() + facet_wrap("trend") +
     xlab("Time") + ylab("")
-  p1
 
+  if (highlight_outliers) {
+    swans <- find_swans(rotated, threshold = threshold)
+    df$outliers <- swans$below_threshold
+    p1 <- p1 + geom_point(data = df[which(df$outliers), ], color = "red")
+  }
+
+  p1
 }
