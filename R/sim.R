edbd657292ebed36fc0f88ba591626a780e1d908
Sean Anderson <sean@seananderson.ca>
2017-08-04 13:00:14 -0700

    Add example simulations

--- R/sim.R
+++ R/sim.R
@@ -20,6 +20,21 @@
 #'   is the true underlying data without observation error added, x is the
 #'   underlying trends, Z is the manipulated loadings matrix that is fed to the
 #'   model.
+#' @examples
+#' x <- sim_dfa()
+#' names(x)
+#' matplot(t(x$y_sim), type = "l")
+#' matplot(t(x$x), type = "l")
+#'
+#' set.seed(42)
+#' x <- sim_dfa(extreme_value = -4, extreme_loc = 10)
+#' matplot(t(x$x), type = "l");abline(v = 10)
+#' matplot(t(x$pred), type = "l");abline(v = 10)
+#'
+#' set.seed(42)
+#' x <- sim_dfa()
+#' matplot(t(x$x), type = "l");abline(v = 10)
+#' matplot(t(x$pred), type = "l");abline(v = 10)
 #'
 #' @export
 
