a94b8929ab0726522cd4ef2e755bed413a45c1c3
Sean Anderson <sean@seananderson.ca>
2017-09-28 16:23:17 -0700

    Allow for a time sliding window

--- R/trend_cor.R
+++ R/trend_cor.R
@@ -6,6 +6,9 @@
 #' @param y A numeric vector to correlate with the DFA trend. Must be the same
 #'   length as the DFA trend.
 #' @param trend A number corresponding to which trend to use.
+#' @param time_window Indices indicating a time window slice to use in the
+#'   correlation. Defaults to using the entire time window. Can be used to walk
+#'   through the timeseries and test the cross correlations.
 #' @param trend_samples The number of samples from the trend posterior to use. A
 #'   model will be run for each trend sample so this value shouldn't be too
 #'   large.
@@ -23,16 +26,23 @@
 #' r <- rotate_trends(m)
 #' correlation <- trend_cor(r, y = rnorm(ncol(r$trends[,1,]), 0, 1))
 #' hist(correlation)
+#' correlation <- trend_cor(r, y = rnorm(ncol(r$trends[,1,]), 0, 1),
+#'   time_window = 5:15)
+#' hist(correlation)
 #' }
 #' @export
 
-trend_cor <- function(rotated_modelfit, y, trend = 1, trend_samples = 100,
+trend_cor <- function(rotated_modelfit, y, trend = 1,
+  time_window = seq_len(length(y)), trend_samples = 100,
   stan_iter = 300, stan_chains = 1, ...) {
 
   # must be even to cleanly divide by 2 later:
   if (!stan_iter %% 2 == 0) stan_iter <- stan_iter + 1
-  y <- as.numeric(scale(y))
-  x <- rotated_modelfit$trends[ , trend, ]
+  if (max(time_window) > length(y)) stop("Maximum time window value is too large")
+
+  y <- as.numeric(scale(y[time_window]))
+  x <- rotated_modelfit$trends[ , trend, time_window] # samples x trend x time
+
   if (!identical(ncol(x), length(y))) stop("DFA trend and y must be same length")
 
   samples <- sample(seq_len(nrow(x)), size = trend_samples)
