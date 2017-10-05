bc23272c587d41d7332e74f240541ac76abc4652
Sean Anderson <sean@seananderson.ca>
2017-10-05 10:56:24 -0700

    Add missing lines

--- R/regime_test.R
+++ R/regime_test.R
@@ -8,6 +8,7 @@ m <- stan("R/cp.stan",
   data = list(r_e = 2.5, r_l = 0.5, T = length(y), D = y),
   cores = 2, chains = 2, iter = 500)
 hist(extract(m)$s)
+x <- extract(m)$expectations
 plot(apply(x, 2, mean), type = "l")
 
 
@@ -17,4 +18,18 @@ m <- stan("R/cp_norm.stan",
   data = list(T = length(y), D = y),
   cores = 2, chains = 2, iter = 500)
 hist(extract(m)$s)
+x <- extract(m)$expectations
 plot(apply(x, 2, mean), type = "l")
+hist(extract(m)$l)
+hist(extract(m)$e)
+hist(extract(m)$sigma)
+
+# y <- c(rnorm(50, 1.5, 0.3), rnorm(50, 0.5, 0.3), rnorm(50, -1, 0.3))
+# plot(y)
+# m <- stan("R/cp_norm_multiple.stan",
+#   data = list(T = length(y), D = y),
+#   cores = 2, chains = 2, iter = 500,
+#   pars = c("e", "l", "m", "s", "expectations"))
+# hist(extract(m)$s)
+# x <- extract(m)$expectations
+# plot(apply(x, 2, mean), type = "l")
