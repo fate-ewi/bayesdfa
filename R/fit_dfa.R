de0fac6ba2857752ef6b8101a160ad2f89cd4199
Sean Anderson <sean@seananderson.ca>
2018-05-03 12:20:09 -0700

    Fix example

--- R/fit_dfa.R
+++ R/fit_dfa.R
@@ -53,7 +53,7 @@
 #'
 #' @examples
 #' set.seed(42)
-#' s <- sim_dfa(num_trends = 2, num_years = 20, num_ts = 3)
+#' s <- sim_dfa(num_trends = 1, num_years = 20, num_ts = 3)
 #' # only 1 chain and 1000 iterations used so example runs quickly:
 #' m <- fit_dfa(y = s$y_sim, iter = 1000, chains = 1)
 
