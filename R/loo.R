29c022ba77d3396cfe5b191aa2e56bc3c1ef0cc5
eric-ward <eric.ward@noaa.gov>
2018-05-09 15:31:07 -0700

    update documentation for loo

--- R/loo.R
+++ R/loo.R
@@ -1,7 +1,16 @@
 #' Return LOO information criteria
 #'
 #' Extract the LOOIC (leave-one-out information criterion) using
-#' [loo::loo()].
+#' [loo::loo()]. Note that we've implemented slightly different variants
+#' of loo, based on whether the DFA observation model includes correlation
+#' between time series or not (default is no correlation). Importantly,
+#' these different versions are not directly comparable to evaluate data support
+#' for including correlation or not in a DFA. If time series are not correlated,
+#' the point-wise log-likelihood for each observation is calculated and used
+#' in the loo calculations. However if time series are correlated, then each
+#' row of the dataframe (time slice) is assumed to be a joint observation of
+#' all variables, and the point-wise log-likelihood is calculated as the
+#' joint likelihood of all variables under the multivariate normal distribution.
 #'
 #' @param fitted_model Output from [fit_dfa()].
 #' @param cores Number of cores to use for parallelization.
