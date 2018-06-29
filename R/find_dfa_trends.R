1c6a232909d554060d673d33be0d1cb7afd465e1
Eric Ward <eric-ward@users.noreply.github.com>
2018-06-29 06:34:31 -0700

    mostly updating for speed

--- R/find_dfa_trends.R
+++ R/find_dfa_trends.R
@@ -19,9 +19,9 @@
 #' @examples
 #' set.seed(42)
 #' s <- sim_dfa(num_trends = 2, num_years = 20, num_ts = 3)
-#' # only 1 chain and 1000 iterations used so example runs quickly:
+#' # only 1 chain and 250 iterations used so example runs quickly:
 #' m <- find_dfa_trends(
-#'   y = s$y_sim, iter = 1000,
+#'   y = s$y_sim, iter = 250,
 #'   kmin = 1, kmax = 2, chains = 1, compare_normal = FALSE,
 #'   variance = "equal", convergence_threshold = 1.1,
 #'   control = list(adapt_delta = 0.95, max_treedepth = 20))
--- R/fit_dfa.R
+++ R/fit_dfa.R
@@ -54,8 +54,8 @@
 #' @examples
 #' set.seed(42)
 #' s <- sim_dfa(num_trends = 1, num_years = 20, num_ts = 3)
-#' # only 1 chain and 1000 iterations used so example runs quickly:
-#' m <- fit_dfa(y = s$y_sim, iter = 1000, chains = 1)
+#' # only 1 chain and 250 iterations used so example runs quickly:
+#' m <- fit_dfa(y = s$y_sim, iter = 250, chains = 1)
 
 fit_dfa <- function(y = y,
                     covar = NULL,
