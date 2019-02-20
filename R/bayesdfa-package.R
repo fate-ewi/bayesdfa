1ced9dc1c4bb48fcd21fad91aa0b70eb3d03e4d2
Eric Ward <eric.ward@noaa.gov>
2019-02-20 13:26:04 -0800

    change package structure, versions / dependencies, using rstantools::create_stan_package

---
+++ R/bayesdfa-package.R
@@ -0,0 +1,16 @@
+#' The 'bayesdfa' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name bayesdfa-package
+#' @aliases bayesdfa
+#' @useDynLib bayesdfa, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
+#'
+NULL
