74a328ca7d2cef6056b47b90d6bf812f1167178a
Eric Ward <eric-ward@users.noreply.github.com>
2021-04-29 13:05:48 -0700

    change up loc of stanfiles
    This largely updates rstantools, putting .stan files in .inst/stan rather than 'src' -- where they had been living

--- R/bayesdfa-package.R
+++ R/bayesdfa-package.R
@@ -1,17 +1,16 @@
 #' The 'bayesdfa' package.
-#' 
+#'
 #' @description A DESCRIPTION OF THE PACKAGE
-#' 
+#'
 #' @docType package
 #' @name bayesdfa-package
 #' @aliases bayesdfa
 #' @useDynLib bayesdfa, .registration = TRUE
 #' @import methods
 #' @import Rcpp
-#' @import rstantools
 #' @importFrom rstan sampling
-#' 
-#' @references 
-#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.18.2. http://mc-stan.org
-#' 
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
 NULL
